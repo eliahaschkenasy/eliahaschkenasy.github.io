27c59dc3b50dd4853ea9bd8aae8232a2f12d9839
eliahaschkenasy <eliahaschkenasy@gmail.com>
2023-04-16 13:27:21 +0300

    new ffmicalc

---
+++ js/ffmicalc.js
@@ -0,0 +1,33 @@
+var heightInput = document.querySelector(".height-input-field");
+var weightInput = document.querySelector(".weight-input-field");
+var fatPercentageInput = document.querySelector(".fat-percentage-input-field")
+var calculateButton = document.querySelector(".calculate");
+var result = document.querySelector(".result");
+var statement = document.querySelector(".result-statement");
+var FFMI, height, weight, fatPercentage;
+
+calculateButton.addEventListener("click", ()=>{
+    
+    height = heightInput.value;
+    weight = weightInput.value;
+    fatPercentage = fatPercentageInput.value;
+    // bodyFat = weight × (fatPercentage / 100)
+    fatFreeMass = weight * (1 - (fatPercentage/ 100)); 
+    FFMI = fatFreeMass/ (height**2);
+    result.innerText = BMI;
+
+    if(isNaN(BMI)) {
+        statement.innerText = "Input Error";
+    }else if(BMI < 18.5){
+        statement.innerText = "Your BMI falls within the underweight range";    
+    }else if((BMI > 18.5) && (BMI < 24.9)){
+        statement.innerText = "Your BMI falls within the normal or healthy weight range";
+    }else if((BMI > 25) && (BMI < 29.9 )){
+        statement.innerText = "Your BMI falls within the overweight range";
+    }else{
+        statement.innerText = "Your BMI falls within the obese range";
+    }
+});
+function toDecimal(value, decimals) {
+    return +value.toFixed(decimals);
+}
